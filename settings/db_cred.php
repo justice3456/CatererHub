e037d29c155c75e739d244471bbef40f22724335
Justice Quagraine <justicequagrainejnr@gmail.com>
2025-03-04 14:57:27 +0000

    updated cred for security

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,7 @@
 <?php
 // Database credentials
-define('SERVER', 'localhost'); 
-define('USERNAME', 'justice.quagraine'); 
-define('PASSWD', '270336'); // Database password
-define('DATABASE', 'webtech_fall2024_justice_quagraine'); // Database name
+define('SERVER', ''); 
+define('USERNAME', ''); 
+define('PASSWD', ''); // Database password
+define('DATABASE', ''); // Database name
 ?>
